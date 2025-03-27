a6ed657f246895e040dcf50548a974c655618563
Paulo Sayao <sayaopaulo@gmail.com>
2025-03-27 15:14:42 -0300

    complete

--- DesafioJavaScript-main/js/form.js
+++ DesafioJavaScript-main/js/form.js
@@ -2,26 +2,45 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, email, telefone, mContato, mensagem) {
+        this.nome = nome,
+        this.email = email,
+        this.telefone = telefone,
+        this.mContato = mContato,
+        this.mensagem = mensagem;
+    }
+dados(){
+    return {
+        "nome": this.nome,
+        "email": this.email,
+        "telefone": this.telefone,
+        "contato":this.mContato,
+        "comentario": this.mensagem
+    }
+}
 }
 
 function Post(form) {
-
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+    let data = new contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("contato").value,
+        form.elements.namedItem("comentario").value,
+    )
+if (data.mContato == 1) {
+    alert("Selecione o motivo do seu contato")
+    return
+}
+Enviar(data)
 }
 
-function Enviar() {
+function Enviar(pessoa) {
+    var nome = pessoa.nome
+    console.table(pessoa.dados())
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    if (nome != "") {
+        alert(`Obrigado sr(a) ${pessoa.nome} os seus dados foram encaminhados com sucesso`);
     }
 
 }
