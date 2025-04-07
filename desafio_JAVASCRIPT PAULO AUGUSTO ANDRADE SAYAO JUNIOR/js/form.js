0e71acd1d717de84911a4f975fd02991946912cd
Paulo Sayao <sayaopaulo@gmail.com>
2025-04-07 15:47:00 -0300

    deixando mais dinamico

--- desafio_JAVASCRIPT PAULO AUGUSTO ANDRADE SAYAO JUNIOR/js/form.js
+++ desafio_JAVASCRIPT PAULO AUGUSTO ANDRADE SAYAO JUNIOR/js/form.js
@@ -48,10 +48,8 @@ function Enviar(pessoa) {
 
 function removeDisabled(el) {
     if (el.checked) {
-        document.getElementById("btnContato").style.background='#1351d8'
         document.getElementById("btnContato").disabled = false
     } else {
-        document.getElementById("btnContato").style.background='#808080'
         document.getElementById("btnContato").disabled = true
     }
 
